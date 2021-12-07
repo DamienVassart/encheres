e1c30132c2d927342e2922e0c95d3e875c97165c
Lydie <lydie.allart@gmail.com>
2021-12-07 18:58:27 +0100

    Categorie class bo

--- src/main/java/fr/eni/encheres/bo/Categorie.java
+++ src/main/java/fr/eni/encheres/bo/Categorie.java
@@ -1,5 +1,37 @@
 package fr.eni.encheres.bo;
 
 public class Categorie {
+	
+	private int noCategorie;
+	private String libelle;
+	
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public Categorie() {
+		super();
+	}
+	public Categorie(int noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+	@Override
+	public String toString() {
+		return "Categorie [libelle=" + libelle + "]";
+	}
+	
+	
+
 
 }
+
