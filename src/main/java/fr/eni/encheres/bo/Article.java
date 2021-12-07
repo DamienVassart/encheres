f2dc726410dd2e0a9e3d4603e47fc4ab56fa9e57
Exzitium <Exzitium@DESKTOP-450P62P.home>
2021-12-07 20:16:52 +0100

    Article classe

--- src/main/java/fr/eni/encheres/bo/Article.java
+++ src/main/java/fr/eni/encheres/bo/Article.java
@@ -1,5 +1,83 @@
 package fr.eni.encheres.bo;
 
+import java.time.LocalDateTime;
+
 public class Article {
 
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDateTime dateDebutEncheres;
+	private LocalDateTime dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private boolean estVendu;
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	public void setDateDebutEncheres(LocalDateTime dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	public LocalDateTime getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	public void setDateFinEncheres(LocalDateTime dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public boolean getEstVendu() {
+		return estVendu;
+	}
+
+	public void setEstVendu(boolean estVendu) {
+		this.estVendu = estVendu;
+	}
+
+	public static void add (Article article) {
+		
+	}
 }
