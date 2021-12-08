73da5b0897e97400f7bada84a548b60946925a20
Exzitium <Exzitium@DESKTOP-450P62P.home>
2021-12-08 15:28:01 +0100

    Correction @Laurane

--- src/main/java/fr/eni/encheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/encheres/dal/UtilisateurDAO.java
@@ -2,6 +2,7 @@ package fr.eni.encheres.dal;
 
 import java.util.List;
 
+import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bo.Utilisateur;
 
 /**
@@ -12,17 +13,17 @@ import fr.eni.encheres.bo.Utilisateur;
 public interface UtilisateurDAO {
 	
 	// S'inscrire
-	public void insert(Utilisateur utilisateur);
+	public void insert(Utilisateur utilisateur) throws BusinessException;
 	
 	// Modifier profil
-	public void update(Utilisateur utilisateur);
+	public void update(Utilisateur utilisateur) throws BusinessException;
 	
 	// Supprimer profil
-	public void  delete(int noUtilisateur);
+	public void  delete(int noUtilisateur) throws BusinessException;
 	
 	//Se Connecter / Afficher Profil
-	public Utilisateur selectById(int noUtilisateur);
+	public Utilisateur selectById(int noUtilisateur) throws BusinessException;
 	
-	public List<Utilisateur> selectAll();
+	public List<Utilisateur> selectAll() throws BusinessException;
 
 }
