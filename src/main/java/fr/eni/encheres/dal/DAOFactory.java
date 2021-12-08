7e05a4a3331243f4c4075b4b8649e7ea98a7d2bf
Exzitium <Exzitium@DESKTOP-450P62P.home>
2021-12-08 15:01:24 +0100

    DAOFactory OK @Laurane

--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -1,9 +1,7 @@
 package fr.eni.encheres.dal;
 
-import java.util.List;
-import java.lang.reflect.*;
-
 import fr.eni.encheres.bo.Article;
+import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * 
@@ -11,86 +9,23 @@ import fr.eni.encheres.bo.Article;
  *
  */
 
-public class DAOFactory implements ArticleDAO {
-
-
-
-	@Override
-	public List<Article> selectAll() {
-		return null;
-	}
-
-	public static ArticleDAO getArticleDAO() {
-		return null;
-
-	}
-
-	@Override
-	public List<Article> selectByName(String nom) {
-		return null;
-	}
-
-	@Override
-	public List<Article> selectByCategory(String categorie) {
-		return null;
-	}
-
-	@Override
-	public void update(Article article) {
-
-	}
-
-	@Override
-	public void delete(int noArticle) {
-
-	}
-
-	@Override
-	public List<Article> selectArticle() {
-		return null;
-	}
-
-	@Override
-	public Article insert(Article article) {
-		return null;
-	}
-
-	@Override
-	public Article select(int idArticle) {
-		return null;
-	}
-
-	@Override
-	public void add(Article article) {
-		
-	}
-
-	@Override
-	public Article selectById(int noArticle) {
-		return null;
-	}
-	private Class ArticleManager;
-	private String articleManager;
+public class DAOFactory  {
 	
-	public DAOFactory(String articleManager) {
-		this.setArticleManager(articleManager);
-		try {
-			setArticleManager(Class.forName(articleManager));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static ArticleDAO getArticleDAO() {
+		return new ArticleDAOJDBCImpl();
 	}
+	
+	public static UtilisateurDAO getUtilisateurDAO() {
+		return new UtilisateurDAOJDBCImpl();
+	}
+	
+	public static RetraitDAO getRetraitDAO() {
+		return new RetraitDAOJDBCImpl();
+	}
+	
+	
 
-	public Class getArticleManager() {
-		return ArticleManager;
-	}
 
-	public void setArticleManager(Class articleManager) {
-		ArticleManager = articleManager;
-	}
 
-	public void setArticleManager(String articleManager) {
-		this.articleManager = articleManager;
-	}
 
 }
