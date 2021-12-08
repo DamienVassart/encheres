d415f328463debc0d6238a33088cfba3bbcfb53b
Lydie <lydie.allart@gmail.com>
2021-12-08 16:43:43 +0100

    @Lydie_bll_UtilisateurManager

--- src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -1,4 +1,10 @@
 package fr.eni.encheres.bll;
+
+import java.sql.SQLException;
+
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
 /**
  * 
  * @author Lydie Allart
@@ -14,4 +20,48 @@ public class UtilisateurManager {
 		}
 		return instance;
 	}
+	
+	private UtilisateurDAO utilisateurDAO;
+	
+
+//Méthodes appelant celles de la DAL
+	
+	//SInscrire
+	public void addUtilisateur(String pseudo, String nom, String prenom, String email, String tel, String rue,
+			int codePostal, String ville, String motDePasse) {
+		utilisateurDAO.addUtilisateur(utilisateurDAO);
+	}
+	//AUTRE A VOIR :
+	//	public void createUtilisateur(String pseudo, String nom, String prenom, String email, String tel, String rue,
+	//			int codePostal, String ville, String motDePasse) {
+	//		utilisateurDAO.createUtilisateur(utilisateurDAO);
+	//	}
+	
+	//ModifierMonProfil
+	public void udpdateUtilisateur(String pseudo, String nom, String prenom, String email, String tel, String rue, 
+			int codePostal, String ville, String motDePasse) throws SQLException {
+		utilisateurDAO.updateUtilisateur(utilisateurDAO);
+	}
+	
+	//SupprimerMonCompte
+	public void removeUtilisateur (int noUtilisateur) {
+		utilisateurDAO.removeUtilisateur(utilisateurDAO);
+	}
+	//AUTRE A VOIR :
+	//	public void deleteUtilisateur(String pseudo, String nom, String prenom, String email, String tel, String rue, 
+	//			int codePostal, String ville, String motDePasse) {
+	//		utilisateurDAO.delete(utilisateurDAO);
+	//	}
+	
+//SeConnecter #AfficherUnProfil
+	//SeConnecter:
+	public Utilisateur seConnecter(String login, String motDePasse) throws SQLException {
+		return utilisateurDAO.selectUtilisateurConnection(login, motDePasse);
+	}	
+	//AfficherUnProfil
+	public Utilisateur getUtilisateur(int noUtilisateur) {
+		return UtilisateurDAO();
+	}
+
 }
+
