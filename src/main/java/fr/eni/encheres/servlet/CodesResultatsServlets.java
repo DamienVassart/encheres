72fcaf1c774460f65f34b3143fc918f8d3756386
Exzitium <Exzitium@DESKTOP-450P62P.home>
2021-12-08 15:45:39 +0100

    Code Resultats Servlet @Laurane

---
+++ src/main/java/fr/eni/encheres/servlet/CodesResultatsServlets.java
@@ -0,0 +1,53 @@
+package fr.eni.encheres.servlet;
+
+/**
+ * 
+ * @author Laurane Marie
+ *
+ */
+
+public abstract class CodesResultatsServlets {
+	
+	// Erreur Format pseudo
+	public static final int FORMAT_PSEUDO_ERREUR = 30000;
+	
+	// Erreur Format nom utilisateur
+	public static final int FORMAT_NOM_UTILISATEUR_ERREUR = 30001;
+
+	// Erreur Format prénom
+	public static final int FORMAT_PRENOM_ERREUR = 30002;
+
+	// Erreur Format mail
+	public static final int FORMAT_EMAIL_ERREUR = 30003;
+
+	// Erreur Format telephone
+	public static final int FORMAT_TEL_ERREUR = 30004;
+
+	// Erreur Format rue
+	public static final int FORMAT_RUE_ERREUR = 30005;
+
+	// Erreur Format code postal
+	public static final int FORMAT_CPO_ERREUR = 30006;
+
+	// Erreur Format ville
+	public static final int FORMAT_VILLE_ERREUR = 30007;
+
+	// Erreur Format mot de passe
+	public static final int FORMAT_MDP_ERREUR = 30008;
+
+	// Erreur Format nom de l'article
+	public static final int FORMAT_NOM_ARTICLE_ERREUR = 30009;
+
+	// Erreur Format description de l'article
+	public static final int FORMAT_DESCRIPTION_ERREUR = 30010;
+	
+	// Erreur Format date de début de l'enchère
+	public static final int FORMAT_DATE_DEBUT_ERREUR = 30011;
+
+	// Erreur Format date de fin de l'enchère
+	public static final int FORMAT_DATE_FIN_ERREUR = 30012;
+
+	// Erreur Format mise a prix
+	public static final int FORMAT_MISE_A_PRIX_ERREUR = 30013;
+
+}
