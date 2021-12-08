88c242a5dd97934436c43f87151ea2d6c00981b3
Lydie <lydie.allart@gmail.com>
2021-12-08 12:32:51 +0100

    modifs_ArticleDAO

--- src/main/java/fr/eni/encheres/dal/ArticleDAO.java
+++ src/main/java/fr/eni/encheres/dal/ArticleDAO.java
@@ -1,17 +1,27 @@
 package fr.eni.encheres.dal;
 
 import java.util.List;
-
 import fr.eni.encheres.bo.Article;
-
+/**
+ * 
+ * @author Lydie Allart
+ *
+ */
 public interface ArticleDAO {
 	
-	public List<Article> selectArticle();
-	
-	
-	
+	//Ajouter l'article
+	public void add(Article article);
+	public List<Article> selectAll();
+	public List<Article> selectByName(String nom);
+	public List<Article> selectByCategory(String categorie);
 	
 	public Article insert(Article article);
-	
+	public Article selectById(int noArticle);
 	public Article select(int idArticle);
+	
+	//Modifier l'article
+	public void update(Article article);
+	
+	//Supprimer l'article
+	public void delete(int noArticle);
 }
