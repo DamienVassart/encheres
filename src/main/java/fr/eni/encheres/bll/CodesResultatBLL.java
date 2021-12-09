2c7112676cfce913d9d2bb2e81f56e0f0d0733c5
dorotheeblanchard <dorotheeblanchard@localhost>
2021-12-09 10:54:01 +0100

    BLL CodesResultatsBLL

---
+++ src/main/java/fr/eni/encheres/bll/CodesResultatBLL.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author Dorothée Blanchard
+ *
+ */
+
+package fr.eni.encheres.bll;
+
+public class CodesResultatBLL {
+	public static final int REGLE_UTILISATEUR_PSEUDO_ERREUR=20000; 
+	public static final int REGLE_UTILISATEUR_NOM_ERREUR=20001;
+	public static final int REGLE_UTILISATEUR_PRENOM_ERREUR=20002;
+	public static final int REGLE_UTILISATEUR_EMAIL_ERREUR=20003;
+	public static final int REGLE_UTILISATEUR_TEL_ERREUR=20004;
+	public static final int REGLE_UTILISATEUR_RUE_ERREUR=20005;
+	public static final int REGLE_UTILISATEUR_CPO_ERREUR=20006;
+	public static final int REGLE_UTILISATEUR_VILLE_ERREUR=20007;
+	public static final int REGLE_UTILISATEUR_MDP_ERREUR=20008;
+	public static final int REGLE_ARTICLE_NOM_ERREUR=20009;
+	public static final int REGLE_ARTICLE_DESCRIPTION_ERREUR=20010;
+	public static final int REGLE_ARTICLE_DATE_DEBUT_ERREUR=20011;
+	public static final int REGLE_ARTICLE_DATE_FIN_ERREUR=20012;
+	public static final int REGLE_ARTICLE_PRIX_ERREUR=20013;
+	public static final int REGLE_RETRAIT_RUE_ERREUR=20014;
+	public static final int REGLE_RETRAIT_CPO_ERREUR=20015;
+
+
+}
