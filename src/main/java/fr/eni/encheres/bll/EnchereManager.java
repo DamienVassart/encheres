ebbe9d03d0af14d2e795a41da14ac7013d9eb54b
DamienVassart <damienvassart@tutanota.com>
2021-12-09 10:32:39 +0100

    EnchereManager complété

--- src/main/java/fr/eni/encheres/bll/EnchereManager.java
+++ src/main/java/fr/eni/encheres/bll/EnchereManager.java
@@ -5,6 +5,7 @@
 package fr.eni.encheres.bll;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bo.Enchere;
@@ -18,27 +19,41 @@ public class EnchereManager {
 		this.enchereDAO = DAOFactory.getEnchereDAO(); // TODO: ajouter méthode getEnchereDAO à DAOFactory (@Laurane)
 	}
 	
-	public void addEnchere(int noUtilisateur, int noArticle, LocalDateTime dateEnchere, int prixVente, int montantEnchere) throws BusinessException {
-		// TODO
+	public void addEnchere(int prixVente, int montantEnchere) throws BusinessException {
+		BusinessException ex = new BusinessException();
+		Enchere enchere = null;
+		
+		validerPrix(prixVente, montantEnchere, ex);
+		
+		if(!ex.hasErreurs()) {
+			enchere = new Enchere();
+			enchere.setDateEnchere(LocalDateTime.now());
+			enchere.setMontantEnchere(montantEnchere);
+		} else {
+			throw ex;
+		}
 	}
 	
+	/*
+	 * TODO: créer EnchereDAO (@Lydie)
+	 */
 	public Enchere getEnchereByNoArticle(int noArticle) throws BusinessException {
-		// TODO
+		return this.enchereDAO.selectByArticle(noArticle);
 	}
 	
-	public Enchere getEnchereByNoUtilisateur(int noUtilisateur) throws BusinessException {
-		// TODO
+	public List<Enchere> getEnchereByNoUtilisateur(int noUtilisateur) throws BusinessException {
+		return this.enchereDAO.selectByUser(noUtilisateur);
 	}
 	
 	public void removeEnchere(int noArticle) throws BusinessException {
-		// TODO
+		this.enchereDAO.delete(noArticle);
 	}
 	
 	/*
 	 * TODO: créer CodesResultatBLL (@Dorothée)
 	 */
-	public void validerPrix(int prixVente, Integer prix, BusinessException ex) throws BusinessException {
-		if(prix == null || prix <= prixVente)
+	public void validerPrix(int prixVente, Integer montantEnchere, BusinessException ex) throws BusinessException {
+		if(montantEnchere == null || montantEnchere <= prixVente)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_ENCHERE_PRIX);
 	}
 }
