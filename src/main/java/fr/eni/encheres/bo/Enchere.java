0fe276f2e0c050251c17bb8bc7873add9891591c
dorotheeblanchard <dorotheeblanchard@localhost>
2021-12-09 08:58:43 +0100

    BO encheres Dorothée

--- src/main/java/fr/eni/encheres/bo/Enchere.java
+++ src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -1,7 +1,50 @@
+/**
+ * 
+ * @author Dorothée Blanchard
+ *
+ */
+
 package fr.eni.encheres.bo;
+
+import java.time.LocalDateTime;
 
 public class Enchere {
 
+	// je commence par établir les propriétés
+	private LocalDateTime dateEnchere; 
+	private Integer montantEnchere;
 	
+	// ensuite les getter setter pour ces propriétés
+	/**
+	 * @return the dateEnchere
+	 */
+	public LocalDateTime getDateEnchere() {
+		return dateEnchere;
+	}
+	/**
+	 * @param dateEnchere the dateEnchere to set
+	 */
+	public void setDateEnchere(LocalDateTime dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+	/**
+	 * @return the montantEnchere
+	 */
+	public Integer getMontantEnchere() {
+		return montantEnchere;
+	}
+	/**
+	 * @param montantEnchere the montantEnchere to set
+	 */
+	public void setMontantEnchere(Integer montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	} 
+	
+	// ne pas oublier le string
+	@Override
+	public String toString() {
+		return null;
+		
+	}
 
 }
