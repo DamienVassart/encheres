4d7cc6d8794edcc87e4ced7d5fe2cf904bb2f916
Lydie <lydie.allart@gmail.com>
2021-12-10 09:56:02 +0100

    @lydie_modifs_UtilisateurManager

--- src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -24,13 +24,13 @@ public class UtilisateurManager {
 		this.utilisateurDAO = DAOFactory.getUtilisateurDAO();
 	}
 	
-	public void insert(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
-					String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur)
+	public void insert(String pseudo, String nom, String prenom, String email, String telephone,
+					String rue, String codePostal, String ville, String motDePasse)
 					throws BusinessException {
 		BusinessException ex = new BusinessException();
 		Utilisateur utilisateur = null;
 		
-		this.validerNoUtilisateur(noUtilisateur, ex);
+		
 		this.validerPseudo(pseudo, ex);
 		this.validerNom(nom, ex);
 		this.validerPrenom(prenom, ex);
@@ -40,11 +40,9 @@ public class UtilisateurManager {
 		this.validerCodePostal(codePostal, ex);
 		this.validerVille(ville, ex);
 		this.validerMotDePasse(motDePasse, ex);
-		this.validerCredit(credit, ex);
-		this.validerAdministrateur(administrateur, ex);
+		
 		if(!ex.hasErreurs()) {
 			utilisateur = new Utilisateur();
-			utilisateur.setNoUtilisateur(noUtilisateur);
 			utilisateur.setPseudo(pseudo);
 			utilisateur.setNom(nom); 
 			utilisateur.setEmail(email); 
@@ -53,8 +51,6 @@ public class UtilisateurManager {
 			utilisateur.setCodePostal(codePostal);
 			utilisateur.setVille(ville);
 			utilisateur.setMotDePasse(motDePasse);
-			utilisateur.setCredit(credit);
-			utilisateur.setAdministrateur(administrateur);
 			
 			Retrait retrait = new Retrait();
 			retrait.setRue(rue);
@@ -68,13 +64,12 @@ public class UtilisateurManager {
 		}
 	}
 	
-	public void update(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
-			String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur)
+	public void update(String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse)
 			throws BusinessException {
 			BusinessException ex = new BusinessException();
 			Utilisateur utilisateur = null;
 
-			this.validerNoUtilisateur(noUtilisateur, ex);
 			this.validerPseudo(pseudo, ex);
 			this.validerNom(nom, ex);
 			this.validerPrenom(prenom, ex);
@@ -84,11 +79,8 @@ public class UtilisateurManager {
 			this.validerCodePostal(codePostal, ex);
 			this.validerVille(ville, ex);
 			this.validerMotDePasse(motDePasse, ex);
-			this.validerCredit(credit, ex);
-			this.validerAdministrateur(administrateur, ex);
+	
 			if(!ex.hasErreurs()) {
-			utilisateur = new Utilisateur();
-			utilisateur.setNoUtilisateur(noUtilisateur);
 			utilisateur.setPseudo(pseudo);
 			utilisateur.setNom(nom); 
 			utilisateur.setEmail(email); 
@@ -97,8 +89,6 @@ public class UtilisateurManager {
 			utilisateur.setCodePostal(codePostal);
 			utilisateur.setVille(ville);
 			utilisateur.setMotDePasse(motDePasse);
-			utilisateur.setCredit(credit);
-			utilisateur.setAdministrateur(administrateur);
 			
 			Retrait retrait = new Retrait();
 			retrait.setRue(rue);
@@ -111,26 +101,11 @@ public class UtilisateurManager {
 			throw ex;
 		}
 	}
-	public void deleteUtilisateur(int noUtilisateur) throws BusinessException {
-		this.utilisateurDAO.delete(noUtilisateur); 
-	}
 	
 	public List<Utilisateur> getUtilisateurs() throws BusinessException {
 		return this.utilisateurDAO.selectAll(); 
 	}
 	
-//	public List<Utilisateur> getUtilisateursByName(String nomUtilisateur) throws BusinessException {
-//		return this.utilisateurDAO.selectByName(nomUtilisateur); 
-//	}
-
-	public Utilisateur getUtilisateur(int noUtilisateur) throws BusinessException {
-		return this.utilisateurDAO.selectById(noUtilisateur); 
-	}
-	
-	private void validerNoUtilisateur(int noUtilisateur, BusinessException ex) {
-		if(noUtilisateur == 0)
-			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_NO_UTILISATEUR?_ERREUR); 
-	}
 	private void validerPseudo(String pseudo, BusinessException ex) {
 		if(pseudo == null || pseudo.trim().equals("") || pseudo.trim().length() > 30)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_PSEUDO_ERREUR); 
@@ -143,49 +118,30 @@ public class UtilisateurManager {
 		if(prenom == null || prenom.trim().equals("") || prenom.trim().length() > 30)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_PRENOM_ERREUR); 
 	}
-	
 	private void validerEmail(String email, BusinessException ex) {
 		if(email == null || email.trim().equals("") || email.trim().length() > 300)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_EMAIL_ERREUR);
 	}
-
 	private void validerTelephone(String telephone, BusinessException ex) {
 		if(telephone == null || telephone.trim().equals("") || telephone.trim().length() > 10)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_TEL_ERREUR);
 	}
-	
 	private void validerRue(String rue, BusinessException ex) {
 		if(rue == null || rue.trim().equals("") || rue.trim().length() > 300)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_RETRAIT_RUE_ERREUR);
 	}
-	
 	private void validerCodePostal(String codePostal, BusinessException ex) {
 		if(codePostal == null || codePostal.trim().equals("") || codePostal.trim().length() > 15)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_RETRAIT_CPO_ERREUR);
 	}
-	
 	private void validerVille(String ville, BusinessException ex) {
 		if(ville == null || ville.trim().equals("") || ville.trim().length() > 30)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_RETRAIT_VILLE_ERREUR);
 	}
-	
-	//Voir la BDD
 	private void validerMotDePasse(String motDePasse, BusinessException ex) {
 		if(motDePasse == null || motDePasse.trim().equals("") || motDePasse.trim().length() > 30)
 			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_MDP_ERREUR);
 	}
-	
-	private void validerCredit(int credit, BusinessException ex) {
-		if(credit == 0)
-			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_CREDIT?_ERREUR);
-	}
-	
-	private void validerAdministrateur(boolean administrateur, BusinessException ex) {
-		if(administrateur == null || administrateur.trim().equals("") || administrateur.trim().length() > 30)
-			ex.ajouterErreur(CodesResultatBLL.REGLE_UTILISATEUR_ADMINISTRATEUR?_ERREUR);
-	}
-}
-}
 
 }
 
