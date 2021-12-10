a9d2ac6b87a9e827f3795fbd7712a32e2cabc212
DamienVassart <damienvassart@tutanota.com>
2021-12-10 15:05:37 +0100

    Ajouté méthode selectByName dans UtilisateurDAO et UtilisateurDAOJDBCImpl

--- src/main/java/fr/eni/encheres/dal/UtilisateurDAOJDBCImpl.java
+++ src/main/java/fr/eni/encheres/dal/UtilisateurDAOJDBCImpl.java
@@ -36,6 +36,8 @@ public class UtilisateurDAOJDBCImpl implements UtilisateurDAO {
 												+ "FROM Utilisateurs;";
 	private static final String SQL_SELECT_BY_ID = "SELECT (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)"
 												+ "FROM Utilisateurs WHERE no_utilisateur = ?;";
+	private static final String SQL_SELECT_BY_NAME = "SELECT (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)"
+												+ "FROM Utilisateurs WHERE nom = ?;";
 	
 	/*
 	 * Insertion d'un nouvel utilisateur (Inscription)
@@ -174,12 +176,46 @@ public class UtilisateurDAOJDBCImpl implements UtilisateurDAO {
 	}
 	
 	/*
-	 * Sélection d'un utilisateur (Afficher un profil, Se Connecter)
+	 * Sélection d'un utilisateur par noUtilisateur (Afficher un profil)
 	 */
 	public Utilisateur selectById(int noUtilisateur) throws BusinessException {
 		Utilisateur utilisateur = new Utilisateur();
 		try (Connection cn = ConnectionProvider.getConnection()) {
 			PreparedStatement ps = cn.prepareStatement(SQL_SELECT_BY_ID);
+			ps.setInt(1, noUtilisateur);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				utilisateur.setPseudo(rs.getString("pseudo"));
+				utilisateur.setNom(rs.getString("nom"));
+				utilisateur.setPrenom(rs.getString("prenom"));
+				utilisateur.setEmail(rs.getString("email"));
+				utilisateur.setTelephone(rs.getString("telephone"));
+				utilisateur.setRue(rs.getString("rue"));
+				utilisateur.setCodePostal(rs.getString("code_postal"));
+				utilisateur.setVille(rs.getString("ville"));
+				utilisateur.setMotDePasse(rs.getString("mot_de_passe"));
+				utilisateur.setCredit(rs.getInt("credit"));
+				utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
+			}
+			
+			rs.close();
+			ps.close();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.SELECT_UTILISATEUR_ECHEC);
+			throw businessException;
+		}
+		return utilisateur;
+	}
+	
+	public Utilisateur selectByName(String nom) throws BusinessException {
+		Utilisateur utilisateur = new Utilisateur();
+		try (Connection cn = ConnectionProvider.getConnection()) {
+			PreparedStatement ps = cn.prepareStatement(SQL_SELECT_BY_NAME);
+			ps.setString(1, nom);
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()) {
 				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
