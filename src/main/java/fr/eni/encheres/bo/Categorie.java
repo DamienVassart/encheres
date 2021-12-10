26a882b9f67354038978c32be0041b6ae13caff9
Lydie <lydie.allart@gmail.com>
2021-12-10 11:22:29 +0100

    @lydie

--- src/main/java/fr/eni/encheres/bo/Categorie.java
+++ src/main/java/fr/eni/encheres/bo/Categorie.java
@@ -1,10 +1,23 @@
 package fr.eni.encheres.bo;
 
+/**
+ * 
+ * @author Lydie Allart
+ *
+ */
 public class Categorie {
 	
 	private int noCategorie;
 	private String libelle;
 	
+	public Categorie() {
+		super();
+	}
+	public Categorie(int noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
 	public int getNoCategorie() {
 		return noCategorie;
 	}
@@ -17,21 +30,12 @@ public class Categorie {
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
 	}
-	public Categorie() {
-		super();
-	}
-	public Categorie(int noCategorie, String libelle) {
-		super();
-		this.noCategorie = noCategorie;
-		this.libelle = libelle;
-	}
+	
 	@Override
 	public String toString() {
 		return "Categorie [libelle=" + libelle + "]";
 	}
 	
-	
-
 
 }
 
