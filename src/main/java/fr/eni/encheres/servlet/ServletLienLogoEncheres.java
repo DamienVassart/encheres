909929d59cb4034392f259893001c96000ed2385
dorotheeblanchard <dorotheeblanchard@localhost>
2021-12-13 11:49:49 +0100

    Servlets - premier brouillon, travail en cours

---
+++ src/main/java/fr/eni/encheres/servlet/ServletLienLogoEncheres.java
@@ -0,0 +1,40 @@
+package fr.eni.encheres.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* 
+* @author Dorothée Blanchard
+* travail en cours
+* configuration par annotation
+* 
+*/
+
+/**
+ * Servlet implementation class ServletLienLogoEncheres
+ */
+@WebServlet("/servlet/ServletLienLogoEncheres") 
+// on conserve cette annotation, on écrit entre parentheses l'url d'acces, par exemple /modules/module2/NomDuFichier
+public class ServletLienLogoEncheres extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Configuration par annotation - Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
