19613d801ec516b7c9eb04157651ee980688b0b3
DamienVassart <damienvassart@tutanota.com>
2021-12-13 15:00:39 +0100

    Modifié ArticleManager

--- src/main/java/fr/eni/encheres/bll/ArticleManager.java
+++ src/main/java/fr/eni/encheres/bll/ArticleManager.java
@@ -25,7 +25,7 @@ public class ArticleManager {
 		this.articleDAO = DAOFactory.getArticleDAO();
 	}
 	
-	public void addArticle(String nomArticle, String description, Date dateDebut, Date dateFin, int miseAPrix, String rue, String codePostal, String ville) throws BusinessException {
+	public void addArticle(String nomArticle, String description, Date dateDebut, Date dateFin, int miseAPrix, String rue, String codePostal, String ville, int noCategorie) throws BusinessException {
 		BusinessException ex = new BusinessException();
 		Article article = null;
 		
@@ -45,6 +45,7 @@ public class ArticleManager {
 			article.setDateFinEncheres(dateFin); 
 			article.setMiseAPrix(miseAPrix);
 			article.setPrixVente(miseAPrix);
+			article.setNoCategorie(noCategorie);
 			
 			Retrait retrait = new Retrait();
 			retrait.setRue(rue);
