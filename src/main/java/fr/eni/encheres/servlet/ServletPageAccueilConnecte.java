31dcab4151ba8a5cd953c18a63f36fb9cf132a75
dorotheeblanchard <dorotheeblanchard@localhost>
2021-12-16 14:56:38 +0100

    ServletPageAccueilConnecte

--- src/main/java/fr/eni/encheres/servlet/ServletPageAccueilConnecte.java
+++ src/main/java/fr/eni/encheres/servlet/ServletPageAccueilConnecte.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/ServletPageAccueil") 
 //on conserve cette annotation, on écrit entre parentheses l'url d'acces, par exemple /modules/module2/NomDuFichier
-public class ServletPageAccueil extends HttpServlet {
+public class ServletPageAccueilConnecte extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	/**
