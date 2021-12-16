4bec078c73bceda0acb00ec3818a41d4979d1bb0
DamienVassart <damienvassart@tutanota.com>
2021-12-16 18:21:23 +0100

    Supprimé commentaires sur ServletSeConnecter

--- src/main/java/fr/eni/encheres/servlet/ServletSeConnecter.java
+++ src/main/java/fr/eni/encheres/servlet/ServletSeConnecter.java
@@ -19,11 +19,9 @@ import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * Servlet implementation class ServletConnexion
+ * @author Laurane Marie
  */
 
-/**
- * Servlet implementation class ServletConnexion
- */
 @WebServlet("/ServletSeConnecter")
 public class ServletSeConnecter extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -46,43 +44,32 @@ public class ServletSeConnecter extends HttpServlet {
 		UtilisateurManager utilisateurManager = new UtilisateurManager();
 		Utilisateur utilisateur = null;
 		
-		// On récupère l'identifiant et le mot de passe
 		String pseudo = request.getParameter("pseudo");
 		String motDePasse = request.getParameter("motDePasse");
 		
-		// Si l'identifiant et / ou le mot de passe sont vides
 		if(pseudo == null || pseudo.trim().equals("") || motDePasse == null || motDePasse.trim().equals("")) {
 			request.setAttribute("errorString", "L'identifiant et le mot de passe sont obligatoires");
 			rd = request.getRequestDispatcher("/WEB-INF/JSP/SeConnecter.jsp");
 		} 
-		// si l'identifiant et le mot de passe ont bien été renseignés
 		else {
-			// appelle la méthode de sélection par pseudo du BLL
 			try {
 				utilisateur = utilisateurManager.getUtilisateurByPseudo(pseudo);
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
-			// si un utilisateur avec ce pseudo est trouvé
 			if(utilisateur != null) {
 				
-				// compare le mot de passe saisi avec celui stocké en BDD
-				// si il est valide
 				if(motDePasse.equals(utilisateur.getMotDePasse())) {
-					/*
-					 * TODO: créer une session ou un cookie
-					 */
+					
 					HttpSession session = request.getSession();
 					rd = request.getRequestDispatcher("/WEB-INF/JSP/PageAccueilConnecte.jsp");
 				} 
-				// sinon
 				else {
 					request.setAttribute("errorString", "Le mot de passe est invalide");
 					rd = request.getRequestDispatcher("/WEB-INF/JSP/SeConnecter.jsp");
 				}
 				
 			} 
-			// si aucun utilisateur avec ce pseudo n'est trouvé
 			else {
 				request.setAttribute("errorString", "Aucun compte associé à cet identifiant");
 				rd = request.getRequestDispatcher("/WEB-INF/JSP/SeConnecter.jsp");
